afcac0363547294cdfa86041ae6817eb1a097db0
WidthDoctor <elskel1812@gmail.com>
2023-05-10 00:22:35 +0200

    fix: fix some problem (Ср, 10 май 2023 00:22:35)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,8 +3,18 @@ const path = require('path');
 
 const folderPath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(folderPath, (files) => {
+fs.readdir(folderPath, (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
   files.forEach((file) => {
+    const filePath = path.join(folderPath, file);
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
       if (stats.isFile()) {
         const fileSizeInBytes = stats.size;
         const fileSizeInKb = fileSizeInBytes / 1024;
@@ -14,3 +24,4 @@ fs.readdir(folderPath, (files) => {
       }
     });
   });
+});
